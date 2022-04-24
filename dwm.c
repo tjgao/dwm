a5713aa841801de81f8f8ead1d09b36762b1a189
Tiejun Gao <tiejun.gao@dolby.com>
2022-04-25 02:14:49 +1000

    Applied swallow patch https://dwm.suckless.org/patches/swallow/dwm-swallow-20201211-61bb8b2.diff

--- dwm.c
+++ dwm.c
@@ -278,6 +278,12 @@ static Client *swallowingclient(Window w);
 static Client *termforwin(const Client *c);
 static pid_t winpid(Window w);
 
+static pid_t getparentprocess(pid_t p);
+static int isdescprocess(pid_t p, pid_t c);
+static Client *swallowingclient(Window w);
+static Client *termforwin(const Client *c);
+static pid_t winpid(Window w);
+
 /* variables */
 static const char autostartblocksh[] = "autostart_blocking.sh";
 static const char autostartsh[] = "autostart.sh";
