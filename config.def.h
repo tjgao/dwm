77c25ed7084eaadc551829a05d15bf2ae25264a3
Tiejun Gao <tiejun.gao@dolby.com>
2022-04-27 10:03:31 +1000

    Adjustment to allow proper awesome font display && border

--- config.def.h
+++ config.def.h
@@ -4,7 +4,7 @@
 #define TERMCLASS "st-256color"
 
 /* appearance */
-static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
@@ -14,8 +14,8 @@ static const unsigned int gappov    = 10;       /* vert outer gap between window
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=16" };
-static const char dmenufont[]       = "monospace:size=16";
+static const char *fonts[]          = {  "UbuntuMono Nerd Font:size=16:antialias=true:autohint=true" };
+static const char dmenufont[]       = "UbuntuMono Nerd Font:size=16:antialias=true:autohint=true";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
