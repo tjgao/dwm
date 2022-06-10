09a65aae71803937838c63a7864211e073f2cec0
Tiejun Gao <tiejun.gao@dolby.com>
2022-06-10 22:50:39 +1000

    minor adjustment

--- config.def.h
+++ config.def.h
@@ -14,7 +14,7 @@ static const unsigned int gappov    = 15;       /* vert outer gap between window
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int user_bh            = 10;        /* 2 is the default spacing around the bar's font */
+static const int user_bh            = 15;        /* 2 is the default spacing around the bar's font */
 static const int vertpad            = 10;       /* vertical padding of bar */
 static const int sidepad            = 10;       /* horizontal padding of bar */
 static const char *fonts[]          = {  "UbuntuMono Nerd Font:size=18:antialias=true:autohint=true" };
