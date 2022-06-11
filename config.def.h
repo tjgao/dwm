61f8079eb09610d1c72b4e39f1e5bbcd6abf958a
Tiejun Gao <tiejun.gao@dolby.com>
2022-06-11 18:30:47 +1000

    Applied status2d patch & minor adjustment

--- config.def.h
+++ config.def.h
@@ -15,11 +15,11 @@ static const int smartgaps          = 0;        /* 1 means no outer gap when the
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 15;        /* 2 is the default spacing around the bar's font */
-static const int vertpad            = 10;       /* vertical padding of bar */
-static const int sidepad            = 10;       /* horizontal padding of bar */
-//static const char *fonts[]          = {  "UbuntuMono Nerd Font:size=18:antialias=true:autohint=true" };
+static const int vertpad            = 15;       /* vertical padding of bar */
+static const int sidepad            = 15;       /* horizontal padding of bar */
 static const char *fonts[]          = {"TerminessTTF Nerd Font:size=18:antialias=true:autohint=true" };
-static const char dmenufont[]       = "UbuntuMono Nerd Font:size=18:antialias=true:autohint=true";
+static const char dmenufont[]       = "TerminessTTF Nerd Font:size=18:antialias=true:autohint=true";
+static const char rofifont[]        = "TerminessTTF Nerd Font 18";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -94,8 +94,8 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char* rofi_runcmd[] = { "rofi", "-show", "run"};
-static const char* rofi_druncmd[] = { "rofi", "-show", "drun"};
+static const char* rofi_runcmd[] = { "rofi", "-font", rofifont, "-show", "run"};
+static const char* rofi_druncmd[] = { "rofi", "-font", rofifont, "-show", "drun"};
 static const char *termcmd[]  = { "st", NULL };
 
 #include "movestack.c"
