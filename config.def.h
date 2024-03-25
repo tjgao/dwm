1fc59bf245bbc728ba7f6c59a971fb9516ed7e35
Tiejun Gao <terry.tiejun.gao@gmail.com>
2024-03-25 16:17:18 +1100

    dwm style tweak

--- config.def.h
+++ config.def.h
@@ -24,11 +24,12 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
+static const char col_pink[]        = "#ffb3d9";
 static const char col_cyan[]        = "#0d212b";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray1, col_pink,  col_pink  },
 };
 
 
@@ -36,8 +37,8 @@ typedef struct {
 	const char *name;
 	const void *cmd;
 } Sp;
-const char *spcmd1[] = {TERMINAL, "-n", "spterm", "-g", "240x68", NULL };
-const char *spcmd2[] = {TERMINAL, "-n", "spfm", "-g", "240x68", "-e", "ranger", NULL };
+const char *spcmd1[] = {TERMINAL, "-n", "spterm", "-g", "190x52", NULL };
+const char *spcmd2[] = {TERMINAL, "-n", "spfm", "-g", "190x52", "-e", "ranger", NULL };
 const char *spcmd3[] = {"keepassxc", NULL };
 static Sp scratchpads[] = {
 	/* name          cmd  */
